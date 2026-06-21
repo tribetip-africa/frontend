c6fa0ee664d8ebbf492db2563b02131eeafb48e8
mboya <mboyaberry@gmail.com>
2026-06-21 14:54:33 +0300

    Reuse shared api-request helper in widget API client.

--- widget/api.ts
+++ widget/api.ts
@@ -1,48 +1,8 @@
-import {
-  handleRequest,
-  parseApiErrorBody,
-  TribetipNetworkError,
-} from "@/lib/errors";
+import { authHeaders, requestJson } from "@/lib/api-request";
 import { getApiBaseUrl } from "@/lib/platform";
-import { secureFetch } from "@/lib/secure-fetch";
 import type { UpdateWidgetEmbedPayload, WidgetEmbedPayload } from "@/widget/types";
 
 const API_BASE = getApiBaseUrl();
-
-function authHeaders(token: string): HeadersInit {
-  return {
-    Accept: "application/json",
-    Authorization: `Bearer ${token}`,
-  };
-}
-
-async function parseJson<T>(response: Response): Promise<T> {
-  const text = await response.text();
-  if (!text) return {} as T;
-  return JSON.parse(text) as T;
-}
-
-async function requestJson<T>(
-  input: string,
-  init: Parameters<typeof secureFetch>[1] = {},
-): Promise<{ response: Response; data: T }> {
-  return handleRequest(async () => {
-    let response: Response;
-
-    try {
-      response = await secureFetch(input, init);
-    } catch (error) {
-      throw new TribetipNetworkError(undefined, error);
-    }
-
-    const data = await parseJson<T & Record<string, unknown>>(response);
-    if (!response.ok) {
-      throw parseApiErrorBody(response.status, data);
-    }
-
-    return { response, data };
-  }, { url: input, method: init.method ?? "GET" });
-}
 
 export async function fetchMyWidgetEmbed(authToken: string): Promise<WidgetEmbedPayload> {
   const { data } = await requestJson<{ widget_embed: WidgetEmbedPayload }>(
